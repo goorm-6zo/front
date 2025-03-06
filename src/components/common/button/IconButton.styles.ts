fc95f125a1e8f10011911ef17eaf9887572c3fe6
funcsom <bishop@kau.kr>
2025-03-06 16:26:44 +0900

    🎨 [DESIGN] IconButton 컴포넌트 제작

---
+++ src/components/common/button/IconButton.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const IconButtonWrapper = styled.button`
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  color: black;
+`;
---
+++ src/components/common/button/IconButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './IconButton.styles.ts';
+
+interface IconButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void; // 클릭 이벤트 핸들러
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ children, onClick }) => {
+  return <S.IconButtonWrapper onClick={onClick}>{children}</S.IconButtonWrapper>;
+};
+
+export default IconButton;
