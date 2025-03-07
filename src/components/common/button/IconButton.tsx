9c00511580a1f98b8d4e982672e53305e781860b
funcsom <bishop@kau.kr>
2025-03-07 16:29:46 +0900

    🎨 design: Button 컴포넌트가 버튼 태그의 기본 속성을 이용할 수 있도록 props 수정

--- src/components/common/button/IconButton.tsx
+++ src/components/common/button/IconButton.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import * as S from './IconButton.style.ts';
 
-interface IconButtonProps {
+type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   onClick?: () => void; // 클릭 이벤트 핸들러
-}
+};
 
 const IconButton: React.FC<IconButtonProps> = ({ children, onClick }) => {
-  return <S.IconButtonWrapper onClick={onClick}>{children}</S.IconButtonWrapper>;
+  return (
+    <S.IconButtonWrapper onClick={onClick}>{children}</S.IconButtonWrapper>
+  );
 };
 
 export default IconButton;
--- src/components/common/button/TextButton.tsx
+++ src/components/common/button/TextButton.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import * as S from './TextButton.style.ts';
 
-interface TextButtonProps {
+type TextButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   onClick?: () => void; // 클릭 이벤트 핸들러
-}
+};
 
 const TextButton: React.FC<TextButtonProps> = ({ children, onClick }) => {
-  return <S.TextButtonWrapper onClick={onClick}>{children}</S.TextButtonWrapper>;
+  return (
+    <S.TextButtonWrapper onClick={onClick}>{children}</S.TextButtonWrapper>
+  );
 };
 
 export default TextButton;
