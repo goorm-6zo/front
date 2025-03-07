57dfbfa12a62c98334b913c21ee77e326b7eddfe
AhnRian <00rian@naver.com>
2025-03-07 15:48:42 +0900

    🦺 Prettier 변경
    prettier 변경

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
 const Home = () => {
-  return <div>HomePage입니다.</div>;
+  return (
+    <Div>
+      <span>h1태그입니다.</span>
+    </Div>
+  );
 };
 
 export default Home;
+
+const Div = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 3px solid blue;
+  padding: 5px;
+  box-sizing: border-box;
+`;
