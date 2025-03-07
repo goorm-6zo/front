cee0b60c6fd4da1d4910f7c70d112c81d11a965d
funcsom <bishop@kau.kr>
2025-03-07 09:27:55 +0900

    🔨 chore: 공통 컴포넌트 활용 페이지 추가 및 라우팅

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/home/Home";
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/home/Home';
+import CommonComponentTest from './pages/home/CommonComponentTest';
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/cct', element: <CommonComponentTest /> },
+]);
 
 export default router;
