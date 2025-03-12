678cf5ae61c46bc02dacc3e7d4d3242da015897c
kkyung99 <becky0508@naver.com>
2025-03-12 21:11:05 +0900

    ✨ feat: 로그인 후 메인 화면에서 유저 정보 가져오기

--- src/api/login/login.ts
+++ src/api/login/login.ts
@@ -9,3 +9,13 @@ export const loginUser = async (data: { email: string; password: string }) => {
     console.log('로그인 실패');
   }
 };
+
+export const getUserData = async () => {
+  try {
+    const response = await API.get('/users');
+    console.log('유저 데이터 조회 성공', response.data);
+    return response.data;
+  } catch (error) {
+    console.log('유저 데이터 조회 실패');
+  }
+};
--- src/pages/user/UserDashboard.tsx
+++ src/pages/user/UserDashboard.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+import { getUserData } from '../../api/login/login';
+
+interface User {
+  name: string;
+  email: string;
+  phone: string;
+}
+
 const UserDashboard = () => {
-  return <div>UserDashboard</div>;
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUserData()
+      .then((data) => {
+        setUserInfo(data);
+      })
+      .catch((error) => {
+        console.log('유저 정보 조회 실패:', error);
+        window.location.href = '/login';
+      });
+  }, []);
+
+  return (
+    <div>
+      메인
+      <div>유저 정보</div>
+      {userInfo ? (
+        <div>
+          <div>이름: {userInfo.name}</div>
+          <div>이메일: {userInfo.email}</div>
+          <div>전화번호: {userInfo.phone}</div>
+        </div>
+      ) : (
+        <span>로그인이 필요합니다!</span>
+      )}
+    </div>
+  );
 };
 
 export default UserDashboard;
