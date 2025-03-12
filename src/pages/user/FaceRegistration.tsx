6201ae22b751ba6b7a76e004a2254440498e178a
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-12 10:06:29 +0900

    [FEAT]: 얼굴 등록 기능 추가 (#19)
    * ♻️ refactor: 얼굴 인식 기능 컴포넌트 분리
    
    * ✨ feat: 얼굴 등록 기능 추가
    
    * 💄 style: 얼굴 인식 화면 styled-component로 변경
    
    * 🎨 design: css 수정

--- src/pages/user/FaceRegistration.tsx
+++ src/pages/user/FaceRegistration.tsx
@@ -1,5 +1,12 @@
+import FaceDetection from '../../components/face/FaceDetection';
+
 const FaceRegistration = () => {
-  return <div>FaceRegistration</div>;
+  return (
+    <div>
+      FaceRegistration
+      <FaceDetection mode="register" />
+    </div>
+  );
 };
 
 export default FaceRegistration;
