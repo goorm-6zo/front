938a9a90b5ea71f5618402bdc8e8d724be867cf8
kkyung99 <becky0508@naver.com>
2025-03-16 19:13:35 +0900

    배포를 위한 테스트

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,7 +8,7 @@ import { loginUser } from '../../api/login/login.ts';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [test, setTest] = useState('');
+  const [test1, setTest1] = useState('');
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
