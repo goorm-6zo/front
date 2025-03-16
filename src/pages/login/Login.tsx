c6de2263203a0a25b9e41149e5397784f295bc15
AhnRian <00rian@naver.com>
2025-03-17 03:52:45 +0900

    cicd 테스트

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,7 +8,6 @@ import { loginUser } from '../../api/login/login.ts';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [test1, setTest1] = useState();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
