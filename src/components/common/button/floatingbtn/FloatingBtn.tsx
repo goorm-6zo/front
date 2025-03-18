3deef6d6dfd1777821fe1120f43cad820901e273
funcsom <bishop@kau.kr>
2025-03-18 15:07:33 +0900

    🎨 design: FloatingBtn 컴포넌트 아이콘 컬러 설정

--- src/components/common/button/floatingbtn/FloatingBtn.tsx
+++ src/components/common/button/floatingbtn/FloatingBtn.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import * as S from './FloatingBtn.style';
 import { Icon } from '../../icon';
+import { useTheme } from 'styled-components';
 
 type FloatingBtnProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
 };
 
 const FloatingBtn: React.FC<FloatingBtnProps> = ({ children, ...props }) => {
+  const theme = useTheme();
+
   return (
     <S.StyledButton {...props}>
-      <Icon name="send" color="white" />
+      <Icon name="send" color={theme.colors.icon.white} />
       {children}
     </S.StyledButton>
   );
