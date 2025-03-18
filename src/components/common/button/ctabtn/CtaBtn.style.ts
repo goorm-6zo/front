0a36f01e039d16188776cde25c0647e8ca7d77e8
funcsom <bishop@kau.kr>
2025-03-18 14:36:37 +0900

    🎨 design: CtnBtn 스타일코드 수정

--- src/components/common/button/ctabtn/CtaBtn.style.ts
+++ src/components/common/button/ctabtn/CtaBtn.style.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 interface ButtonProps {
   variant: 'primary' | 'secondary' | 'tertiary';
-  state: 'default' | 'pressed' | 'disabled';
 }
 
 export const StyledButton = styled.button<ButtonProps>`
@@ -10,35 +9,16 @@ export const StyledButton = styled.button<ButtonProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 12px 12px;
+  padding: var(--spacing-12) var(--spacing-12);
   font: var(--font-title-l);
   border-radius: var(--radius-12);
-  cursor: ${({ state }) => (state === 'disabled' ? 'not-allowed' : 'pointer')};
-  opacity: ${({ state }) => (state === 'disabled' ? '0.5' : '1')};
-  transition:
-    background-color 0.2s ease,
-    border 0.2s ease;
 
-  ${({ theme, variant, state }) => {
-    const isPressed = state === 'pressed';
-
+  ${({ theme, variant }) => {
     switch (variant) {
       case 'primary':
         return `
-          background-color: ${
-            state === 'disabled'
-              ? theme.colors.background.secondary
-              : isPressed
-                ? theme.colors.background.primaryPressed
-                : theme.colors.background.primary
-          };
-          color: ${
-            state === 'disabled'
-              ? theme.colors.typo.disabled
-              : isPressed
-                ? theme.colors.typo.weak
-                : theme.colors.typo.white
-          };
+          background-color: ${theme.colors.background.primary};
+          color: ${theme.colors.typo.white};
           border: none;
 
           &:active {
@@ -54,46 +34,22 @@ export const StyledButton = styled.button<ButtonProps>`
         `;
       case 'secondary':
         return `
-          background-color: ${
-            state === 'disabled'
-              ? theme.colors.background.secondaryPressed
-              : isPressed
-                ? theme.colors.background.secondaryPressed
-                : theme.colors.background.white
-          };
+          background-color: ${theme.colors.background.white};
           color: ${theme.colors.typo.secondary};
           border: 1px solid ${theme.colors.border.secondary};
 
           &:active {
             background-color: ${theme.colors.background.secondaryPressed};
           }
-
-          &:disabled {
-            pointer-events: none;  /* 클릭 이벤트 방지 */
-            background-color: ${theme.colors.background.secondaryPressed};
-            color: ${theme.colors.typo.disabled};
-          }
         `;
       case 'tertiary':
         return `
-          background-color: ${
-            state === 'disabled'
-              ? theme.colors.background.secondaryPressed
-              : isPressed
-                ? theme.colors.background.secondaryPressed
-                : theme.colors.background.secondary
-          };
+          background-color: ${theme.colors.background.secondary};
           color: ${theme.colors.typo.secondary};
           border: none;
 
           &:active {
             background-color: ${theme.colors.background.secondaryPressed};
-          }
-
-          &:disabled {
-            pointer-events: none;  /* 클릭 이벤트 방지 */
-            background-color: ${theme.colors.background.secondaryPressed};
-            color: ${theme.colors.typo.disabled};
           }
         `;
       default:
