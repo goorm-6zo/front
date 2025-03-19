a83d61b81004a5c4fbaba8651640ba558f3e0125
kkyung99 <becky0508@naver.com>
2025-03-19 15:28:35 +0900

    🔨 chore: 불필요한 코드 제거

--- src/components/common/checkbox/Checkbox.tsx
+++ src/components/common/checkbox/Checkbox.tsx
@@ -18,23 +18,7 @@ export const Checkbox = ({
     <S.CheckboxContainer>
       <S.HiddenInput type="checkbox" checked={checked} onChange={onChange} />
       <S.CustomCheckbox checked={checked}>
-        {checked && (
-          <Icon name="success" />
-          //   <svg
-          //     xmlns="http://www.w3.org/2000/svg"
-          //     width="17"
-          //     height="16"
-          //     viewBox="0 0 17 16"
-          //     fill="none"
-          //   >
-          //     <rect
-          //       x="0.5"
-          //       width="16"
-          //       height="16"
-          //       fill={checked ? '#00E6FF' : 'transparent'}
-          //     />
-          //   </svg>
-        )}
+        {checked && <Icon name="success" />}
       </S.CustomCheckbox>
       <S.CheckboxLabel>{children}</S.CheckboxLabel>
     </S.CheckboxContainer>
