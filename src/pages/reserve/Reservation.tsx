cedfae5ec7b5df3c1e620ac97e2d9af8bd05b444
AhnRian <00rian@naver.com>
2025-03-19 15:54:19 +0900

    ✨ feat: 예약 페이지 API 통신

--- src/pages/reserve/Reservation.tsx
+++ src/pages/reserve/Reservation.tsx
@@ -1,11 +1,15 @@
 import * as S from './Reservation.style';
 import Layout from '../../components/common/layout/Layout';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { createReservation } from '../../api/reserve/createReservation';
+import { getConferenceInfo } from '../../api/reserve/getConferenceInfo';
+
+//components
 import { Input } from '../../components/common/input/Input';
 import CtaButton from '../../components/common/button/CtaButton';
 import CheckBoxList from '../../components/reservation/session/CheckBoxList';
 
-//dummy data1
 const checkBoxItems = [
   { id: 1, label: '첫 번째 체크박스', checked: false },
   { id: 2, label: '두 번째 체크박스', checked: true },
@@ -16,7 +20,54 @@ const checkBoxItems = [
 ];
 
 const Reservation = () => {
-  const [hasSession, _setHasSession] = useState(true);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [sessionIds, setSessionIds] = useState([]);
+  const [hasSession, setHasSession] = useState(true);
+  const conferenceId = 1;
+
+  const handleReservation = async () => {
+    if (!name || !phone) {
+      alert('이름과 전화번호를 입력해주세요.');
+      return;
+    }
+
+    try {
+      const reserveData = {
+        conferenceId,
+        sessionIds,
+        name,
+        phone,
+      };
+      const response = await createReservation(reserveData);
+      console.log('예약 성공:', response);
+      alert('예약이 완료되었습니다.');
+    } catch (error) {
+      console.error('예약 실패:', error);
+      alert('예약에 실패했습니다.');
+    }
+  };
+
+  useEffect(() => {
+    const fetchConferenceInfo = async () => {
+      try {
+        const data = await getConferenceInfo(conferenceId);
+        console.log(data);
+        console.log('컨퍼런스 정보:', data.sessions);
+        setHasSession(data.hasSessions);
+        if (data.hasSessions) {
+          const sessionIds = data.sessions.map(
+            (session: { id: number }) => session.id,
+          );
+          setSessionIds(sessionIds);
+        }
+      } catch (error) {
+        console.error('컨퍼런스 조회 실패:', error);
+      }
+    };
+
+    fetchConferenceInfo();
+  }, []);
 
   return (
     <Layout hasHeader={false} hasFooter={false}>
@@ -27,29 +78,36 @@ const Reservation = () => {
         </S.ReservationTitle>
         <S.ReservationForm id="reservation-form">
           {hasSession && <CheckBoxList items={checkBoxItems} />}
-          {/* 예매자 이름 */}
+
           <S.InputDiv>
             <Input
               title="예매자"
               state="default"
               type="text"
               placeholder="이름"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </S.InputDiv>
 
-          {/* 전화번호 */}
           <S.InputDiv>
             <Input
               title="전화번호"
               state="default"
               type="tel"
               placeholder="전화번호"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
             />
           </S.InputDiv>
         </S.ReservationForm>
 
         <S.BtnWrapper>
-          <CtaButton type="submit" variant="primary" form="reservation-form">
+          <CtaButton
+            type="button"
+            variant="primary"
+            onClick={handleReservation}
+          >
             예약하기
           </CtaButton>
         </S.BtnWrapper>
