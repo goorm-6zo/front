7ab39c6a963d0c886910103d2a589a9d7656d127
AhnRian <125647933+AhnRian@users.noreply.github.com>
2025-03-20 00:15:30 +0900

    [DESIGN]: 웹 공통 컴포넌트 수정 (#61)
    * 🎨 design: profile 컴포넌트 반응형 추가
    
    * 🔨 chore: img 폴더 위치 변경
    
    * 🎨 design: Img 컴포넌트 size 숫자로 변경
    
    * 🎨 design: Ani 컴포넌트 반응형 추가
    
    * 🎨 design: Badge컴포넌트 반응형 추가

--- src/components/common/ani/Ani.tsx
+++ src/components/common/ani/Ani.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import * as S from './Ani.style';
-
 type AniProps = {
-  size?: number;
   imageUrl?: string;
 };
-const Ani: React.FC<AniProps> = ({ size = 130, imageUrl }) => {
+const Ani: React.FC<AniProps> = ({ imageUrl }) => {
   return (
-    <S.AniContainer size={size}>
+    <S.AniContainer>
       {imageUrl && <S.Ani src={imageUrl} alt="애니메이션 이미지" />}
     </S.AniContainer>
   );
