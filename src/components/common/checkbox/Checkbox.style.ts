1e635c451185801acdf6aa4e6c253ee10b3faad6
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-20 03:22:40 +0900

    🎨 design: checkbox 컴포넌트 ui 및 상호작용 수정 (#63)

--- src/components/common/checkbox/Checkbox.style.ts
+++ src/components/common/checkbox/Checkbox.style.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
-import { CheckboxProps } from './Checkbox';
 
-export const CheckboxContainer = styled.label`
-  display: flex;
-  position: relative;
+export const CheckboxContainer = styled.span`
+  display: inline-flex;
+  flex-direction: row;
   align-items: center;
   gap: var(--spacing-8);
+  margin-right: var(--spacing-8);
+`;
+
+export const CheckboxLabel = styled.label`
+  position: relative;
   user-select: none;
-  justify-content: center;
 `;
 
 export const HiddenInput = styled.input`
@@ -17,30 +20,7 @@ export const HiddenInput = styled.input`
   height: 0;
 `;
 
-export const CustomCheckbox = styled.span<CheckboxProps>`
-  width: 20px;
-  height: 20px;
-  border-radius: 2px;
-  border: ${({ checked, theme }) =>
-    checked
-      ? `2px solid ${theme.colors.border.tertiary}`
-      : `2px solid ${theme.colors.border.primary}`};
-  background: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  transition: all 0.2s ease-in-out;
-
-  &:active {
-    border: ${({ theme }) => theme.colors.border.primaryPressed};
-    background: ${({ checked, theme }) =>
-      checked
-        ? theme.colors.background.primaryPressed
-        : theme.colors.background.secondaryPressed};
-  }
-`;
-
-export const CheckboxLabel = styled.span`
-  font: var(--font-body-l);
+export const CheckboxText = styled.span`
+  font: var(--font-body-m);
+  user-select: none;
 `;
