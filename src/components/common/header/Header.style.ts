72220e312f6b62d34a3f17ba6e5238dba10e585c
funcsom <bishop@kau.kr>
2025-03-20 11:28:34 +0900

    🎨 design: (예시) Header 컴포넌트 제작 (반응형 고려)

--- src/components/common/header/Header.style.ts
+++ src/components/common/header/Header.style.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import { media } from '../../../styles/breakpoints';
 
 export const HeaderContainer = styled.header`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: white;
-  padding: var(--spacing-36) var(--spacing-16);
+  background-color: ${({ theme }) => theme.colors.background.white};
   position: relative;
+
+  ${media.mobile} {
+    padding: var(--spacing-16) var(--spacing-20);
+  }
+  ${media.desktop} {
+    padding: var(--spacing-16) var(--spacing-44);
+  }
 `;
