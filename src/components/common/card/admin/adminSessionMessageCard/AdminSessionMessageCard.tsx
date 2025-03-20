d4c9184e87406a410d28871be931aa6c33ee81f9
funcsom <bishop@kau.kr>
2025-03-20 17:42:21 +0900

    🎨 design: admin-AdminSessionMessageCard 컴포넌트 제작

---
+++ src/components/common/card/admin/adminSessionMessageCard/AdminSessionMessageCard.tsx
@@ -0,0 +1,36 @@
+import * as S from './AdminSessionMessageCard.style';
+import Profile from '../../profile/Profile';
+import Btn from '../../../button/btn/Btn';
+import { Tag } from '../../../tag/Tag.style';
+
+type AdminSessionMessageCardProps = {
+  title: string;
+  name: string;
+  from: string;
+  state?: 'default' | 'done';
+};
+
+const AdminSessionMessageCard: React.FC<AdminSessionMessageCardProps> = ({
+  title,
+  name,
+  from,
+  state = 'default',
+}) => {
+  return (
+    <S.CardContainer>
+      <S.ContentsContainer>
+        <S.HeaderContainer>
+          <S.TagContainer>
+            <Tag variant="primary">구역 A</Tag>
+            <Tag variant="secondary">시간</Tag>
+            <Tag variant="secondary">장소</Tag>
+          </S.TagContainer>
+          <S.TitleWrapper>{title}</S.TitleWrapper>
+        </S.HeaderContainer>
+        <Profile name={name} from={from} />
+      </S.ContentsContainer>
+    </S.CardContainer>
+  );
+};
+
+export default AdminSessionMessageCard;
