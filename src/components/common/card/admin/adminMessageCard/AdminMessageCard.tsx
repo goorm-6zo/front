151eb44f1e2fbc2d42d9ce6609821ffaecf5d0d6
funcsom <bishop@kau.kr>
2025-03-20 20:53:52 +0900

    🎨 design: admin-AdminMessageCard 컴포넌트 제작

---
+++ src/components/common/card/admin/adminMessageCard/AdminMessageCard.tsx
@@ -0,0 +1,35 @@
+import * as S from './AdminMessageCard.style';
+import Profile from '../../profile/Profile';
+import { Tag } from '../../../tag/Tag.style';
+
+type AdminMessageCardProps = {
+  title: string;
+  name: string;
+  from: string;
+  state?: 'default' | 'done';
+};
+
+const AdminMessageCard: React.FC<AdminMessageCardProps> = ({
+  title,
+  name,
+  from,
+  state = 'default',
+}) => {
+  return (
+    <S.CardContainer>
+      <S.ContentsContainer>
+        <S.HeaderContainer>
+          <S.TagContainer>
+            <Tag variant="primary">구역 A</Tag>
+            <Tag variant="secondary">시간</Tag>
+            <Tag variant="secondary">장소</Tag>
+          </S.TagContainer>
+          <S.TitleWrapper>{title}</S.TitleWrapper>
+        </S.HeaderContainer>
+        <Profile name={name} from={from} />
+      </S.ContentsContainer>
+    </S.CardContainer>
+  );
+};
+
+export default AdminMessageCard;
