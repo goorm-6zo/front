8506e79aa3c1b0633b0051fab57c309fda32a60c
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-21 21:06:38 +0900

    🐛 fix: 특정 컨퍼런스 api 호출 로직 수정 (#84)

--- src/api/admin/conference/getConferenceInfo.ts
+++ src/api/admin/conference/getConferenceInfo.ts
@@ -1,8 +1,8 @@
 import API from '../../axiosIntance';
 
-export const getConferenceInfo = async () => {
+export const getConferenceInfo = async (conferenceId: number) => {
   try {
-    const response = await API.get('/admin/conference/1');
+    const response = await API.get(`/admin/conference/${conferenceId}`);
     return response.data;
   } catch (error) {
     console.log('컨퍼런스 조회 실패', error);
