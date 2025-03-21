a92597419dac90ff1f1663304e4cdfc8174e8326
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-21 16:58:01 +0900

    [DESIGN]: 로그인 페이지 UI 수정 및 라우터 수정 (#80)
    * 🔨 chore: 로고 추가
    
    * 🔨 chore: .gitkeep 삭제
    
    * 🎨 design: 카카오 로고 추가 및 카카오 로그인 버튼 스타일 수정
    
    * 💄 style: TxtBtn 스타일 수정
    
    * 🎨 design: 메인 로그인 페이지 ui 구현
    
    * 🐛 fix: 로그인 라우터 및 PrivateRoute 수정 (메인 페이지 경로 변경)
    
    * ✨ feat: 이메일 로그인 버튼 클릭 시 페이지 이동
    
    * 🎨 design: 로그인 페이지 ui 수정
    
    * ✨ feat: 이미 로그인한 유저 로그인 페이지 접근 제한 추가
    
    * 🔨 chore: 주석 제거
    
    * 🔨 chore: 라우팅 관련 파일 폴더화 및 위치 변경
    
    * 🐛 fix: 경로 오류 수정

--- src/components/common/button/ctabtn/CtaBtn.style.ts
+++ src/components/common/button/ctabtn/CtaBtn.style.ts
@@ -14,7 +14,6 @@ export const StyledButton = styled.button<ButtonProps>`
   font: var(--font-title-l);
   border-radius: var(--radius-12);
   cursor: pointer;
-  width: 100%;
 
   ${({ theme, $variant }) => {
     switch ($variant) {
