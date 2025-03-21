b821154335023d56909a9388151ab23a55a52af9
AhnRian <125647933+AhnRian@users.noreply.github.com>
2025-03-21 21:49:26 +0900

    🎨 design: 관리자 대시보드 UI (#85)

--- src/pages/admin/adminDashboard/AdminDashboard.tsx
+++ src/pages/admin/adminDashboard/AdminDashboard.tsx
@@ -1,25 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getConferenceInfo } from '../../../api/admin/conference/getConferenceInfo';
 import * as S from './AdminDashboard.style';
+import AdminSessionCard from '../../../components/common/card/admin/adminSessionCard/AdminSessionCard';
+import ResponsiveLayout from '../../../components/common/layout/ResponsiveLayout';
+type sessionType = {
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+  conferenceAt: string;
+  capacity: number;
+  hasSessions: boolean;
+  endTime: string;
+  conferenceId: number;
+  speakerImage: string | null;
+  speakerName: string | null;
+  speakerOrganization: string | null;
+};
 
-// type confType = {
-//   id: number;
-//   name: string;
-//   description: string;
-//   location: string;
-//   conferenceAt: string;
-//   capacity: string;
-//   hasSessions: boolean;
-// };
 const AdminDashboard = () => {
-  // const [confDatas, setConfDatas] = useState<confType[]>([]);
+  const [confDatas, setConfDatas] = useState<sessionType[]>([]);
 
   useEffect(() => {
     const fetchConferences = async () => {
       try {
         const res = await getConferenceInfo(1);
         console.log('컨퍼런스 데이터:', res);
-        // setConfDatas(res);
+        setConfDatas(res.sessions);
       } catch (error) {
         console.error('컨퍼런스 데이터 조회 실패', error);
       }
@@ -30,25 +37,22 @@ const AdminDashboard = () => {
 
   //임시 디자인
   return (
-    <S.Container>
-      <S.Title>관리자 대시보드</S.Title>
-      {/* <S.CardGrid>
-        {confDatas.map((conf) => (
-          <S.ConferenceCard key={conf.id}>
-            <S.CardTitle>{conf.name}</S.CardTitle>
-            <S.CardInfo>📍 장소: {conf.location}</S.CardInfo>
-            <S.CardInfo>
-              📅 일정: {new Date(conf.conferenceAt).toLocaleDateString()}
-            </S.CardInfo>
-            <S.CardInfo>👥 최대 참석: {conf.capacity}명</S.CardInfo>
-            <S.CardInfo>
-              {conf.hasSessions ? '✅ 세션 포함' : '❌ 세션 없음'}
-            </S.CardInfo>
-            <S.CardDescription>{conf.description}</S.CardDescription>
-          </S.ConferenceCard>
-        ))}
-      </S.CardGrid> */}
-    </S.Container>
+    <ResponsiveLayout>
+      <S.Container>
+        <S.TitleBox>
+          <S.Title>행사 목록</S.Title>
+          <S.SubTitle>
+            얼굴 인증으로 입출입을 관리하는 행사 목록이에요
+          </S.SubTitle>
+        </S.TitleBox>
+        <S.DataBox>
+          {confDatas &&
+            confDatas.map(() => (
+              <AdminSessionCard title="제목" name="이름" from="from" />
+            ))}
+        </S.DataBox>
+      </S.Container>
+    </ResponsiveLayout>
   );
 };
 
