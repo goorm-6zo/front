830e6f3fda7589225a0c7891c4bd1abd3f1bb877
funcsom <bishop@kau.kr>
2025-03-22 10:32:41 +0900

    🎨 design: Signup 페이지 제작

---
+++ src/pages/login/Signup.style.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-24);
+
+  & > * {
+    flex-shrink: 0;
+  }
+`;
+
+export const PageTitleWrapper = styled.div`
+  display: flex;
+  padding: var(--spacing-20) var(--spacing-0);
+  font: var(--font-title-xl);
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-12);
+`;
+
+export const AgreeCardContainer = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.border.primary};
+  border-radius: var(--radius-16);
+  margin-top: 68px;
+  padding: var(--spacing-12) var(--spacing-0);
+`;
+
+export const AgreeAllWrapper = styled.div`
+  padding: var(--spacing-12) var(--spacing-0);
+  border-bottom: 1px solid ${({ theme }) => theme.colors.divider.primary};
+`;
+
+export const AgreeAllContainer = styled.div`
+  padding: var(--spacing-0) var(--spacing-20);
+`;
+
+export const AgreePortionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: var(--spacing-20);
+  gap: var(--spacing-12);
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-28);
+`;
--- src/pages/login/Signup.tsx
+++ src/pages/login/Signup.tsx
@@ -1,5 +1,12 @@
 import { useForm } from 'react-hook-form';
 import { signup } from '../../api/login/signup';
+import { Input } from '../../components/common/input/Input';
+import ResponsiveLayout from '../../components/common/layout/ResponsiveLayout';
+import * as S from './Signup.style';
+import CtaBtn from '../../components/common/button/ctabtn/CtaBtn';
+import { Checkbox } from '../../components/common/checkbox/Checkbox';
+import Footer from '../../components/common/footer/Footer';
+
 const SignUp = () => {
   const { register, handleSubmit } = useForm();
 
@@ -14,32 +21,61 @@ const SignUp = () => {
   };
 
   return (
-    <div style={{ maxWidth: '300px', margin: 'auto', textAlign: 'center' }}>
-      <h2>회원가입</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('name')} placeholder="이름" required />
-        <input
-          {...register('email')}
-          type="text"
-          placeholder="이메일"
-          required
-        />
-        <input
-          {...register('password')}
-          type="password"
-          placeholder="비밀번호"
-          required
-        />
+    <ResponsiveLayout hasHeader={false}>
+      <S.PageContainer>
+        <S.PageTitleWrapper>회원가입</S.PageTitleWrapper>
+        <S.FormContainer onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('name')} placeholder="이름" required />
+          <input
+            {...register('email')}
+            type="text"
+            placeholder="이메일"
+            required
+          />
+          <input
+            {...register('phone')}
+            type="tel"
+            placeholder="전화번호"
+            required
+          />
+          <input
+            {...register('password')}
+            type="password"
+            placeholder="비밀번호"
+            required
+          />
+          <input
+            {...register('password')}
+            type="password"
+            placeholder="비밀번호"
+            required
+          />
+          <S.AgreeCardContainer>
+            <S.AgreeAllContainer>
+              <S.AgreeAllWrapper>
+                <Checkbox checked={false} label="전체동의" />
+              </S.AgreeAllWrapper>
+            </S.AgreeAllContainer>
 
-        <input
-          {...register('phone')}
-          type="tel"
-          placeholder="전화번호"
-          required
-        />
-        <button type="submit">가입하기</button>
-      </form>
-    </div>
+            <S.AgreePortionWrapper>
+              <Checkbox
+                variant="secondary"
+                label="(필수) 서비스 이용약관에 동의합니다."
+                onChange={() => {}}
+              />
+              <Checkbox
+                variant="secondary"
+                label="(필수) 개인정보 수집•이용에 동의합니다."
+                onChange={() => {}}
+              />
+            </S.AgreePortionWrapper>
+          </S.AgreeCardContainer>
+          <Footer>
+            <CtaBtn type="submit">가입하기</CtaBtn>
+          </Footer>
+        </S.FormContainer>
+      </S.PageContainer>
+    </ResponsiveLayout>
   );
 };
 
