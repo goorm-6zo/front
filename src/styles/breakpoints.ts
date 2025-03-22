967ea081cec4fd5c7777fbdd36497f06d1b39bb7
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-22 18:22:35 +0900

    💄 style: 미디어 쿼리 설정 변경 (#88)

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -11,8 +11,9 @@ const media = {
   expandedLayout: `@media (min-width: ${breakpoints.expandedMin})`,
 
   // 컴포넌트에 적용할 쿼리 헬퍼 함수
-  mobile: `@media (max-width: ${breakpoints.desktopMin})`,
+  mobile: `@media (min-width: ${breakpoints.mobileMin})`,
   desktop: `@media (min-width: ${breakpoints.desktopMin})`,
+  expanded: `@media (min-width: ${breakpoints.expandedMin})`,
 };
 
 export { breakpoints, media };
