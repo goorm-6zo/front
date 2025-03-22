73cb8fafc664e7499c2b71c7e7b39161cb2e66b4
kkyung99 <becky0508@naver.com>
2025-03-22 22:20:53 +0900

    ✨ feat: 컨퍼런스와 세션 데이터 표시 및 카드 컴포넌트 UI 구현

---
+++ src/components/common/card/admin/adminEntryCard/AdminEntryCard.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import { getConferenceInfo } from '../../../../../api/reserve/getConferenceInfo';
+import * as S from './AdminEntryCard.style.ts';
+import { Tag } from '../../../tag/Tag.tsx';
+import Icon from '../../../icon/Icon.tsx';
+
+interface ConferenceInfo {
+  id: number;
+  name: string;
+  startTime: string;
+  attend: number;
+  sessions: {
+    id: number;
+    name: string;
+    startTime: string;
+  }[];
+}
+
+export default function AdminEntryCard() {
+  const [conferInfo, setConferInfo] = useState<ConferenceInfo | null>(null);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchConference = async () => {
+      const response = await getConferenceInfo(1);
+      if (response) {
+        setConferInfo(response);
+      }
+    };
+    fetchConference();
+
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
+
+  const sortedSessions = conferInfo?.sessions.sort((a, b) => a.id - b.id);
+
+  return (
+    <>
+      {conferInfo ? (
+        <S.CardContainer>
+          <S.Card>
+            <S.CardHeader>
+              <S.TotalCount>총 500명</S.TotalCount>
+              <S.CornerBox>
+                <Icon
+                  name="rightArrow"
+                  color="#909298"
+                  size={isMobile ? 20 : 24}
+                />
+              </S.CornerBox>
+            </S.CardHeader>
+            <S.EntryCount>
+              {conferInfo.attend ? conferInfo.attend : 0}명 입장
+            </S.EntryCount>
+            <Tag isEntryStatus={true}>{conferInfo.name}</Tag>
+          </S.Card>
+
+          {sortedSessions?.map((session) => (
+            <S.Card key={session.id}>
+              <S.CardHeader>
+                <S.TotalCount>총 0명</S.TotalCount>
+                <S.CornerBox>
+                  <Icon
+                    name="rightArrow"
+                    color="#909298"
+                    size={isMobile ? 20 : 24}
+                  />
+                </S.CornerBox>
+              </S.CardHeader>
+              <S.EntryCount>0명 입장</S.EntryCount>
+              <Tag isEntryStatus={true}>{session.name}</Tag>
+            </S.Card>
+          ))}
+        </S.CardContainer>
+      ) : (
+        <div>없음</div>
+      )}
+    </>
+  );
+}
