763d570e198d5d6547bb61b561ad83a8a0ec2a6f
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-22 22:36:54 +0900

    [FEAT]: 컨퍼런스 카드 컴포넌트 구현 및 입장 현황 페이지 적용 (#89)
    * 🎨 design: right arrow 아이콘 추가
    
    * 🎨 design: Tag 컴포넌트 스타일 수정
    
    * 💄 style: --font-title-xl-2 폰트 크기 추가
    
    * 💄 style: font size 추가
    
    * ✨ feat: 컨퍼런스와 세션 데이터 표시 및 카드 컴포넌트 UI 구현
    
    * ✨ feat: 입장 현황 페이지 구현

--- src/components/common/tag/Tag.style.ts
+++ src/components/common/tag/Tag.style.ts
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 import { TagProps } from './Tag';
 
 export const Tag = styled.span<TagProps>`
-  display: inline-flex;
+  display: block;
   font: var(--font-caption-s);
   padding: var(--spacing-4) var(--spacing-12);
   border-radius: var(--radius-9999);
+  text-align: center;
+  width: ${({ isEntryStatus }) => (isEntryStatus ? '100%' : 'auto')};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 
   ${({ variant, theme }) => {
     switch (variant) {
--- src/components/common/tag/Tag.tsx
+++ src/components/common/tag/Tag.tsx
@@ -4,8 +4,17 @@ import * as S from './Tag.style';
 export interface TagProps {
   variant?: 'primary' | 'secondary' | 'tertiary';
   children: React.ReactNode;
+  isEntryStatus?: boolean;
 }
 
-export const Tag = ({ variant = 'primary', children }: TagProps) => {
-  return <S.Tag variant={variant}>{children}</S.Tag>;
+export const Tag = ({
+  variant = 'primary',
+  children,
+  isEntryStatus,
+}: TagProps) => {
+  return (
+    <S.Tag variant={variant} isEntryStatus={isEntryStatus}>
+      {children}
+    </S.Tag>
+  );
 };
