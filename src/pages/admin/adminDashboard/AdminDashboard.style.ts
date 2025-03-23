37fb2d84b746c7dc228db472649006077b70da29
kkyung99 <becky0508@naver.com>
2025-03-23 17:40:25 +0900

    🔨 chore: 사용하지 않는 스타일 삭제

--- src/pages/admin/adminDashboard/AdminDashboard.style.ts
+++ src/pages/admin/adminDashboard/AdminDashboard.style.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
-export const Container = styled.div`
-  max-width: 1200px;
-`;
+
 export const TitleBox = styled.div`
   padding: var(--spacing-20) 0;
   display: flex;
