00c68a04e09adb285a782a8d47be47407bdf7801
funcsom <bishop@kau.kr>
2025-03-23 10:36:46 +0900

    🎨 design: Icon 등록

---
+++ src/assets/svg/IcnStrokeMenu.tsx
@@ -0,0 +1,3 @@
+const IcnStrokeMenu = () => {};
+
+export default IcnStrokeMenu;
---
+++ src/assets/svg/IcnStrokePlus.tsx
@@ -0,0 +1,19 @@
+const IcnStrokePlus = () => {
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M4.28784 12H19.7122M12 4.28784V19.7122"
+      stroke="#414244"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>;
+};
+
+export default IcnStrokePlus;
