596c35d47f156cfb8e4d71aa8320b42cd3f71cd9
funcsom <bishop@kau.kr>
2025-03-25 08:41:29 +0900

    🎨 design: Sheet 컴포넌트 스타일링

--- src/components/common/sheet/SheetBtn.style.ts
+++ src/components/common/sheet/SheetBtn.style.ts
@@ -1,11 +1,27 @@
 import styled from 'styled-components';
 
-export const StyledButton = styled.button`
+interface StyledButtonProps {
+  state: 'on' | 'off';
+}
+
+export const StyledButton = styled.button<StyledButtonProps>`
   flex: 1;
   display: flex;
   align-items: center;
-  justify-content: center;
-  padding: var(--spacing-20) var(--spacing-16);
+  justify-content: flex-start;
+  padding: var(--spacing-20) var(--spacing-24);
   font: var(--font-title-xl);
+  margin: var(--spacing-0) var(--spacing-20);
+  border: none;
   border-radius: var(--radius-12);
+  cursor: pointer;
+
+  ${({ state, theme }) =>
+    state === 'on'
+      ? `
+      background-color: ${theme.colors.background.secondary};
+    `
+      : `
+      background-color: ${theme.colors.background.white};
+    `}
 `;
