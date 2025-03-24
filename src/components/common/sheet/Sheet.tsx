d7876cae2fed208367dd3d0d134101da12605bae
funcsom <bishop@kau.kr>
2025-03-24 14:21:39 +0900

    🎨 design: sheet 컴포넌트 제작

--- src/components/common/sheet/Sheet.tsx
+++ src/components/common/sheet/Sheet.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import IconButton from '../button/IconButton';
 import * as S from './Sheet.style';
 import Icon from '../icon/Icon';
+import Btn from '../button/btn/Btn';
+import IcnBtn from '../button/icnbtn/IcnBtn';
+import SheetBtn from './SheetBtn';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { logoutUser } from '../../../api/login/logout';
+useLocation;
 
 interface SheetProps {
   isOpen: boolean;
@@ -9,21 +14,52 @@ interface SheetProps {
 }
 
 const Sheet: React.FC<SheetProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const currentPath = location.pathname;
+
+  const handleLogout = async () => {
+    const response = await logoutUser();
+    if (response) {
+      console.log('로그아웃 성공');
+      navigate('/login');
+    } else {
+      console.log('로그아웃 실패');
+    }
+  };
 
   return (
-    <S.SideSheetOverlay $isOpen={isOpen}>
-      <S.SideSheetContainer
-        onClick={(e) => e.stopPropagation()}
-        $isOpen={isOpen}
-      >
+    <S.SideSheetOverlay onClick={onClose} $isOpen={isOpen}>
+      <S.SideSheetContainer $isOpen={isOpen}>
         <S.CloseButtonWrapper>
-          <IconButton onClick={onClose}>
+          <IcnBtn onClick={onClose}>
             <Icon name="strokeclose" />
-          </IconButton>
+          </IcnBtn>
         </S.CloseButtonWrapper>
-        <S.MenuItem>HOME</S.MenuItem>
-        <S.MenuItem>MY</S.MenuItem>
+        <S.ContentsWrapper>
+          <S.ButtonWrapper>
+            <SheetBtn
+              onClick={() => navigate('/dashboard')}
+              state={currentPath === '/profile' ? 'off' : 'on'}
+            >
+              HOME
+            </SheetBtn>
+            <SheetBtn
+              state={currentPath === '/profile' ? 'on' : 'off'}
+              onClick={() => navigate('/profile')}
+            >
+              MY
+            </SheetBtn>
+          </S.ButtonWrapper>
+
+          <S.BottomButtonWrapper>
+            <Btn variant="quaternary">공지사항</Btn>
+            <Btn variant="quaternary" onClick={handleLogout}>
+              로그아웃
+            </Btn>
+          </S.BottomButtonWrapper>
+        </S.ContentsWrapper>
       </S.SideSheetContainer>
     </S.SideSheetOverlay>
   );
