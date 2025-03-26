5d4305b956a2dd7492c12e1a8425b33221256498
funcsom <bishop@kau.kr>
2025-03-26 10:25:10 +0900

    🔨 chore: Checkbox 스타일 수정

--- src/components/common/checkbox/Checkbox.style.ts
+++ src/components/common/checkbox/Checkbox.style.ts
@@ -66,7 +66,14 @@ export const CircleInput = styled.input<StyledCircleProps>`
   }
 `;
 
-export const IconWrapper = styled.div``;
+export const IconWrapper = styled.div`
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  pointer-events: none;
+`;
 
 export const CheckboxText = styled.span`
   font: var(--font-body-m);
