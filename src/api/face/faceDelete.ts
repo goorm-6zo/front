1b6f228f6f2766160f9aea03cbf19cc089198c35
funcsom <bishop@kau.kr>
2025-03-27 00:05:35 +0900

    ✨ feat: 등록된 얼굴 데이터 삭제 api

---
+++ src/api/face/faceDelete.ts
@@ -0,0 +1,11 @@
+import API from '../axiosIntance';
+
+export const faceDelete = async () => {
+  try {
+    const response = await API.delete('/face/delete');
+
+    return response.data.data;
+  } catch (error) {
+    console.log('등록한 얼굴 삭제 실패', error);
+  }
+};
