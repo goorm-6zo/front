18325cefaf9bed7d2d77bb810417b3c175c615f1
funcsom <bishop@kau.kr>
2025-03-28 00:02:23 +0900

    🔨 chore: 이메일 중복체크 로직 수정

--- src/api/login/isEmailAvailable.ts
+++ src/api/login/isEmailAvailable.ts
@@ -6,7 +6,6 @@ export const isEmailAvailable = async (email: string) => {
       email: email,
     });
 
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log('이메일 중복체크 실패', error); // true: 사용 가능 / false: 중복
