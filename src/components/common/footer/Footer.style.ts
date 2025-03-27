5a86520434deead85fc6fc9834af8e98ff96edca
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-03-28 03:37:47 +0900

    [FEAT]: 메시지 전송 API 연동 (#157)
    * ✨ feat: 메시지 API 연동하여 메시지 전송 기능 추가
    
    * 💄 style: footer 스타일 그림자 제거

--- src/components/common/footer/Footer.style.ts
+++ src/components/common/footer/Footer.style.ts
@@ -12,7 +12,6 @@ export const FooterContainer = styled.footer`
   align-items: center;
   z-index: 1;
   background-color: ${({ theme }) => theme.colors.background.white};
-  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
 
   ${media.mobile} {
     padding: var(--spacing-16) var(--spacing-20) 52px var(--spacing-20);
--- src/pages/admin/MessageSend.style.ts
+++ src/pages/admin/MessageSend.style.ts
@@ -123,3 +123,29 @@ export const CloseBtn = styled.span`
   cursor: pointer;
   z-index: 1;
 `;
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.9);
+  z-index: 1000;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledToast = styled.div`
+  position: fixed;
+  bottom: var(--spacing-20);
+  left: 50%;
+  transform: translateX(-50%);
+  padding: var(--spacing-10) var(--spacing-20);
+  color: white;
+  border-radius: var(--radius-4);
+  z-index: 2000;
+`;
