e0d3f9d749c2bc517de87f2a5e43e58351f5722f
funcsom <bishop@kau.kr>
2025-03-27 14:45:30 +0900

    🔨 chore: isEmailAvailable api 수정

--- src/api/login/isEmailAvailable.ts
+++ src/api/login/isEmailAvailable.ts
@@ -1,6 +1,15 @@
 import API from '../axiosIntance';
 
-export const isEmailAvailable = async (email: string): Promise<boolean> => {
-  const res = await API.post('/auth/check-email', { email });
-  return res.data.available; // true: 사용 가능 / false: 중복
+export const isEmailAvailable = async (email: string) => {
+  try {
+    const response = await API.get('/users/check-email', {
+      params: {
+        email: email,
+      },
+    });
+
+    return response.data.data;
+  } catch (error) {
+    console.log('이메일 중복체크 실패', error); // true: 사용 가능 / false: 중복
+  }
 };
