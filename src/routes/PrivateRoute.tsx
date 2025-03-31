9f1b958a3f2e492f7af8d5ab7e7b46a6fe0f5def
funcsom <bishop@kau.kr>
2025-03-31 11:24:25 +0900

    🐛 fix: 본인인증 안된 사람이 dashboard로 넘어가는것을 막기 위한 코드 삭제

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,15 +1,15 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStore } from '../store/useAuthStore';
 
 export default function PrivateRoute({ role }: { role: 'USER' | 'ADMIN' }) {
   const { userInfo } = useAuthStore();
-  const location = useLocation();
+  // const location = useLocation();
 
   if (!userInfo) return <Navigate to="/" replace />;
 
-  if (!userInfo.phone && location.pathname !== '/authorization/callback') {
-    return <Navigate to="/authorization/callback?hasPhone=false" replace />;
-  }
+  // if (!userInfo.phone && location.pathname !== '/authorization/callback') {
+  //   return <Navigate to="/authorization/callback" replace />;
+  // }
 
   if (userInfo.role !== role) {
     return (
