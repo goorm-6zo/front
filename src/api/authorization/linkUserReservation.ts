842d1386ec14643d494e36312a864b7d039cba3e
성소민 <162946302+funcsom@users.noreply.github.com>
2025-03-31 10:36:15 +0900

    🐛 fix: linkUserReservation API 연결 추가하고 본인인증 onSubmit에 API 호출하기 (#190)

---
+++ src/api/authorization/linkUserReservation.ts
@@ -0,0 +1,13 @@
+import API from '../axiosIntance';
+
+export const linkUserReservation = async (phone: string) => {
+  try {
+    const response = await API.post('/reservation/link-user', {
+      phone: phone,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log('유저 예약내역 연결 실패', error);
+  }
+};
