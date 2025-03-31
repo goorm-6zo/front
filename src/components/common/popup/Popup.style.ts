68274f72bcd0ec058fe0896dfa9f20da571cea21
성소민 <162946302+funcsom@users.noreply.github.com>
2025-04-01 00:02:26 +0900

    🎨 design: popup 오버레이 수정 (#202)

--- src/components/common/popup/Popup.style.ts
+++ src/components/common/popup/Popup.style.ts
@@ -10,7 +10,7 @@ export const PopupOverlay = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 2;
+  z-index: 600;
 `;
 
 export const PopupContent = styled.div`
